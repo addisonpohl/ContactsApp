bf9ef3517b4779694a5638bd532c5171c789edd2
addisonpohl <addison.c.pohl@gmail.com>
2019-07-10 22:15:16 -0500

    Add create action

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -5,6 +5,16 @@ class Api::ContactsController < ApplicationController
     render "index.json.jbuilder"
   end
 
+  def create
+    @contact = Contact.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      phone_number: params[:phone_number],
+      email: params[:email]
+    )
+    render "show.json.jbuilder"
+  end
+
   def show
     @contact = Contact.find_by(id: params[:id])
     render "show.json.jbuilder"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_30_022243) do
+ActiveRecord::Schema.define(version: 2019_07_11_025128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_06_30_022243) do
   create_table "contacts", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.integer "phone_number"
+    t.bigint "phone_number"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
