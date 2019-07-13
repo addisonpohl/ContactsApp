311f685ce791ce05c75b4070c6f834b7d04273da
addisonpohl <addison.c.pohl@gmail.com>
2019-07-12 23:25:27 -0500

    Add update action and route

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -19,4 +19,13 @@ class Api::ContactsController < ApplicationController
     @contact = Contact.find_by(id: params[:id])
     render "show.json.jbuilder"
   end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.phone_number = params[:phone_number] || @contact.phone_number
+    @contact.email = params[:email] || @contact.email
+    render "show.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
     get "/contacts" => "contacts#index"
     get "/contacts/:id"=> "contacts#show"
     post "/contacts" => "contacts#create"
+    put "/contacts/:id" => "contacts#update"
   end
 end
